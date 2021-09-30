ce4120e452dad40cad5b75b8bbfcf8a1a5d02e6b
tsurenko <valera.tsurenko228@gmail.com>
2021-09-30 21:28:23 +0300

    fix: classes in html/js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
     // Navigation arrows
     navigation: {
-      nextEl: '.slider-button--next',
-      prevEl: '.slider-button--prev',
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
     },
 });
