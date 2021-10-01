0c469e91a3220670f8aa59318f8ddccdf01b1db2
tsurenko <valera.tsurenko228@gmail.com>
2021-10-01 20:58:34 +0300

    creart: mdia for reviews

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
-const swiper = new Swiper('.hotel-slider', {
+const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
     // Navigation arrows
     navigation: {
       nextEl: '.hotel-slider__button--next',
       prevEl: '.hotel-slider__button--prev',
+    },
+});
+const reviewsSlider =  new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider-button--next',
+      prevEl: '.reviews-slider-button--prev',
     },
 });
