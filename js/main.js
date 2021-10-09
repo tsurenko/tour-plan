a77499f633bc37ad12c1ed05cae7724a28eb39b6
tsurenko <valera.tsurenko228@gmail.com>
2021-10-09 20:39:47 +0300

    create: mobile menu/ fixed: Css Grid

--- js/main.js
+++ js/main.js
@@ -16,3 +16,11 @@ const reviewsSlider =  new Swiper('.reviews-slider', {
       prevEl: '.reviews-slider-button--prev',
     },
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик по кнопке меню");
+  document
+    .querySelector(".navbar-buttom")
+    .classList.toggle("navbar-buttom--visible");
+});
