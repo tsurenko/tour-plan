534e556d8c0fc7cea768ed1e3fffe2edd3e0dc26
tsurenko <valera.tsurenko228@gmail.com>
2021-10-15 21:22:12 +0300

    create: modal/thankyou.html&fix:media

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const hotelSlider = new Swiper('.hotel-slider', {
+$( document ).ready( function () {
+  const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
     // Navigation arrows
@@ -6,7 +7,7 @@ const hotelSlider = new Swiper('.hotel-slider', {
       nextEl: '.hotel-slider__button--next',
       prevEl: '.hotel-slider__button--prev',
     },
-});
+  });
 const reviewsSlider =  new Swiper('.reviews-slider', {
   // Optional parameters
   loop: true,
@@ -15,7 +16,7 @@ const reviewsSlider =  new Swiper('.reviews-slider', {
       nextEl: '.reviews-slider-button--next',
       prevEl: '.reviews-slider-button--prev',
     },
-});
+  });
 
 var menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener("click", function () {
@@ -23,4 +24,24 @@ menuButton.addEventListener("click", function () {
   document
     .querySelector(".navbar-buttom")
     .classList.toggle("navbar-buttom--visible");
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close")
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass('modal__dialog--visible');
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
 });
