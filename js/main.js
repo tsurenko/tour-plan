2a1fb5d2e00e87ab5b663094c962f725383434ad
tsurenko <valera.tsurenko228@gmail.com>
2021-10-17 19:46:14 +0300

    craete:validation for forms & fix: media footer

--- js/main.js
+++ js/main.js
@@ -44,4 +44,25 @@ menuButton.addEventListener("click", function () {
     modalOverlay.removeClass('modal__overlay--visible');
     modalDialog.removeClass('modal__dialog--visible');
   }
+
+  $(".form").each(function(){
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+        required: "Please specify your name",
+        minlenght:  "Name must be at least 2 letters",
+      },
+      email: {
+        required: "We need your email address to contact you",
+        email: "Your email address must be in the format of name@domain.com",
+      },
+      phone: {
+      required: "Enter your phone,please",
+      },
+    },
+    });
+  });
+  $("#phone_1").mask("+7(999) 999-9999");
+  $("#phone_2").mask("+7(999) 999-9999");
 });
